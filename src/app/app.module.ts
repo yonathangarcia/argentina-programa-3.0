47d6cb3918e6028b3c63db8020882ed5b23ffe3e
Yonathan Exequiel Garcia <garcia.yonathan@outlook.com.ar>
2023-03-25 20:41:18 -0300

    components y assets
    components header, social, logo, banner y assets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoComponent } from './components/logo/logo.component';
+import { SocialComponent } from './components/social/social.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoComponent,
+    SocialComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
